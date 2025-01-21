a9d2683a143aee950b166a894741abbfb564324d
Я <koalder@yandex.ru>
2025-01-21 10:30:50 +0300

    Завершил занятие 29

--- blog/blogapp/views.py
+++ blog/blogapp/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, redirect
 from django.urls import reverse, reverse_lazy
+from django.conf import settings
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView # Базовые классы
 from django.views.generic.base import ContextMixin # Для создание общих классов
 
@@ -33,10 +36,12 @@ def contact_view(request):
         form = ContactForm()
         return render(request, 'blogapp/create.html', context={'form': form})
 
+@user_passes_test(lambda u: u.is_superuser) # Теперь, при @user_passes_test смотреть пост разрешено только ползователям с определённым условием (здесь только админ)
 def post(request, id):
     post = get_object_or_404(Post, id=id)
     return render(request, 'blogapp/post.html', context={'post': post})
 
+@login_required # Вот так просто добавляем это и посты смогут создавать только залогиненные пользователи
 def create_post(request):
     if request.method == 'GET':
         form = PostForm() # Из forms.py
@@ -45,6 +50,7 @@ def create_post(request):
         form = PostForm(request.POST, # Передаём данные, которые сюда придут
                         files=request.FILES) # Если есть изображения или файлы, то ещё и это прописываем
         if form.is_valid(): # Если данные формы заполнены правильно
+            form.instance.user = request.user # Сохраняем в БД таблице текущего пользователя
             form.save() # Все данные, поля помнит, поэтому их загрузит и заполнит в БД
             return HttpResponseRedirect(reverse('blogapp:index'))  # Перенаправляем на главную страницу
         else: # Если данные формы заполнены неправильно, то загрузит прежднюю страницу с формой для заполнения
@@ -65,9 +71,20 @@ class TagListView(ListView, NameContextMixin):
         return Tag.objects.all() # В таком виде обычно возвращает (по-умолчанию), а можно настроить (переопределить), например, на получение определённых данных
 
 # Детальная информация
-class TagDetailView(DetailView, NameContextMixin):
+class TagDetailView(LoginRequiredMixin, # Оказывается ещё и это нужно, чтобы был редирект (на занятиях не показали)
+                    UserPassesTestMixin, # Для функциональности @user_passes_test, но в виде класса, однако условия разрешения нужно писать тут ниже в def test_func(self)
+                    DetailView, NameContextMixin):
     model = Tag
     template_name = 'blogapp/tag_detail.html'
+    login_url = settings.LOGIN_URL  # Используем значение из settings.py, чтобы редирект был (на занятиях не показали)
+
+    def test_func(self): # См. выше про UserPassesTestMixin
+        return self.request.user.is_superuser # Да, решение при использовании классов по-длиннее получается
+
+    def dispatch(self, request, *args, **kwargs): # А также нужно вот так переопределить этот метод, чтобы был редирект при неподходящем логине (тоже в занятиях не показали)
+        if not self.test_func():
+            return redirect(self.login_url)  # Редирект на страницу входа
+        return super().dispatch(request, *args, **kwargs)
 
     # Эти 3 функции сделаны так, что ничего не меняют, но в дальнейшем можно понастроить и изменить где надо, если не устраивают имена по-умолчанию
     def get(self, request, *args, **kwargs): # Базовый get-функция переопределения
@@ -78,7 +95,8 @@ class TagDetailView(DetailView, NameContextMixin):
                                  pk=self.tag_id) # Имя поля ключа снова такая же (демонстрируется в учебных целях)
 
 # Создание тэга
-class TagCreateView(CreateView, NameContextMixin): # Вместо подобной громадной def create_post(request)
+class TagCreateView(LoginRequiredMixin, # Чтобы теги мог создавать только залогиненный, !!! причём LoginRequiredMixin должен 1-м идти
+                    CreateView, NameContextMixin): # Вместо подобной громадной def create_post(request)
     model = Tag
     # form_class =
     fields = '__all__' # Выбираем все поля класса Tag
@@ -89,6 +107,7 @@ class TagCreateView(CreateView, NameContextMixin): # Вместо подобно
 
     def form_valid(self, form): # Метод срабатывает после того, как выясняется, что форма правильная
         # Кроме того, можно с этой формой тут делать всякое
+        # form.instance.user = self.request.user # Если в классах (для примера), то вот так сохраняем в таблице БД текущего пользователя
         return super().form_valid(form)
 
 
