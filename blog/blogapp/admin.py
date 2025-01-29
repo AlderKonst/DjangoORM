568a64c016b9d2ee2d5872e23b2f50bf64d8fa49
Я <koalder@yandex.ru>
2025-01-29 13:10:33 +0300

    Чуть админку изменил

--- blog/blogapp/admin.py
+++ blog/blogapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Category, Post, Tag
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin): # Для расширения возможностей админки
+    list_display = ['name', 'text', 'category'] # Поля в виде колонок с возможностью сортировки
 
 admin.site.register(Category)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
